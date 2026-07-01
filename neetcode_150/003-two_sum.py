05f0b85dcf7d7830760c95e6e26c3a9b3608c13a
Nguyen Le Gia Huy <huyng.otis@gmail.com>
2026-07-01 21:25:20 +0700

    Solve: Two Sum

---
+++ neetcode_150/003-two_sum.py
@@ -0,0 +1,38 @@
+# Problem: Two Sum
+# Difficulty: Easy
+# Link NeetCode: https://neetcode.io/problems/two-integer-sum/question?list=neetcode150 
+# Link LeetCode: https://leetcode.com/problems/two-sum/description/
+
+# Brief of problem 
+# Giving an array and a value
+# Return the indicies of two numbers which add up to value
+# Only have one solution -> Only need return a pair of indicies for every problem (any order)
+
+from typing import List
+
+# Easy Approach: Brute Force
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if (nums[i] + nums[j] == target):
+                    return [i, j]
+
+# Result: AC
+# Time Complexity: O(n^2)
+
+# More optimal Approach: Using a hash map
+
+import bisect
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dct = {}
+        for i in range(nums):
+            if (dct.get(target - nums[i], -1) != -1):
+                return [dct[target - nums[i]], i]
+            dct[nums[i]] = i
+
+# Result: AC
+# Time Complexity: O(n)
