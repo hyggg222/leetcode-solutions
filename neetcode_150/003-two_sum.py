0319a2a5c05b897c8594ba4291b147c2b55bc827
Nguyen Le Gia Huy <huyng.otis@gmail.com>
2026-07-01 21:33:45 +0700

    update

--- neetcode_150/003-two_sum.py
+++ neetcode_150/003-two_sum.py
@@ -22,6 +22,21 @@ class Solution:
 # Result: AC
 # Time Complexity: O(n^2)
 
+# Harder Approach: Two pointer
+# Create a copy of the array and sort it in ascending order -> initilize two pointers, at the beginning and at the end
+# -> Iterate through the array and check if the sum of the two numbers is equal to the target
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        arr = nums
+        arr.sort()
+        i = 0
+        j = len(arr) - 1
+        while i < j:
+            if (arr[i] + arr[j] == target):
+                return [i, j]
+            elif   :
+    
 # More optimal Approach: Using a hash map
 
 import bisect
