4676ec2f83087bded8a3483432b764125b86443a
Nguyen Le Gia Huy <huyng.otis@gmail.com>
2026-07-02 19:52:31 +0700

    better sol

--- neetcode_150/004-group_anagrams.py
+++ neetcode_150/004-group_anagrams.py
@@ -27,13 +27,56 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         grouped = [0] * len(strs)
         ans = []
-        for i, char in enumerate(strs):
+        for i, Str in enumerate(strs):
             if (grouped[i] == 0):
-                ls = [char]
+                ls = [Str]
                 for j in range(i + 1, len(strs)):
-                    if self.check_ana(strs[j], char):
+                    if self.check_ana(strs[j], Str):
                         ls.append(strs[j])
                         grouped[j] = 1    
                 ans.append(ls)        
         return ans
 
+# Time complexity: O(n^2 + k)
+
+# Better Approach
+# Sort all the strings n the array (ana become the same string) -> Sort the array (ana are next to each others) 
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        arr = []
+        for i, Str in enumerate(strs):
+            arr.append([sorted(Str), i])
+        arr.sort()
+        ans = []
+        ls = [strs[arr[0][1]]]
+        for i in range(1, len(strs)):
+            if (arr[i][0] == arr[i-1][0]):
+                ls.append(strs[arr[i][1]])
+            else:
+                ans.append(ls)
+                ls = [strs[arr[i][1]]]
+        ans.append(ls)
+        return ans
+# Time complexity: O(nklogk)
+
+# Using a hash map: <key>: dict of frequency of character - <value>: list of strings
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dct = {}
+        for Str in strs:
+            dt = {}
+            for char in Str:
+                dt[char] = dt.get(char, 0) + 1
+            key = tuple(sorted(dt.items()))
+            if key not in dct:
+                dct[key] = []
+            dct[key].append(Str)
+        return list(dct.values())
+
+
+# Time complexity: O(nk)
+
+
+        
