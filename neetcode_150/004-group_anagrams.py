317984ae1e34ac7830eaf85ef121f628e1e57067
Nguyen Le Gia Huy <huyng.otis@gmail.com>
2026-07-02 19:12:14 +0700

    prbl

---
+++ neetcode_150/004-group_anagrams.py
@@ -0,0 +1,11 @@
+# Problem: Group Anagrams
+# Difficulty: Medium
+# Link NeetCode: https://neetcode.io/problems/anagram-groups/question?list=neetcode150
+# Link LeetCode: https://leetcode.com/problems/group-anagrams/
+
+# Brief of problem
+
+# group the anagrams together -> anagrams are strings which have the same frequency of every contained characters
+
+# Easy Approach: Brute Force 
+
