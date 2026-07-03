fe87f41b134a0f0e5f9e342546444dce6893f847
Nguyen Le Gia Huy <huyng.otis@gmail.com>
2026-07-03 20:38:35 +0700

    005: Optimal Sol

--- neetcode_150/005-top_k_frequent_elements.py
+++ neetcode_150/005-top_k_frequent_elements.py
@@ -43,4 +43,26 @@ class Solution:
         return ans
 
 # Time Complexity: O(N * LogK)
+
+# More Optimal Sol: Distribution Count
+# Optimize the way we count the top largest -> Solution (Watch later)
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
+
+
         
