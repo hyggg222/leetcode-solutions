28e46e83f28ac6bf8c212428cef17e176a1cf45e
Nguyen Le Gia Huy <huyng.otis@gmail.com>
2026-07-08 20:29:05 +0700

    Solve optimal 009

--- 009-LCS.py
+++ 009-LCS.py
@@ -44,5 +44,30 @@ class Solution:
 obj = Solution()
 nums=[2,20,4,10,3,4,5]
 print(obj.longestConsecutive(nums))
+
+# Optimal Approach: Using a hash set
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        num_set = set(nums)
+        ans = 0
+
+        for num in num_set:
+            if (num - 1) in num_set: 
+                continue
+            cur_num = num
+            cur_streak = 1
+            while (cur_num + 1) in num_set:
+                cur_streak += 1
+                cur_num += 1
+            ans = max(ans, cur_streak)
+        
+        return ans
+    
+# Another Solution:
+
+                
+
+
             
         
