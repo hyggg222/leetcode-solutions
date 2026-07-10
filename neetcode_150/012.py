4cc7df3c96e9851cbc6505720f100f061dd51d1e
Nguyen Le Gia Huy <huyng.otis@gmail.com>
2026-07-10 19:39:19 +0700

    Solve 012: Optimal Solution

--- neetcode_150/012.py
+++ neetcode_150/012.py
@@ -54,3 +54,39 @@ class Solution:
 # Result: Accepted
 
 # Insight: We should minimize the memory usage -> get risk of data structures
+# Can use the idea of 2sum: Sort and iterate through every value -> Find other two
+# value that equals to (Target (0) - First Value).
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        ans = []
+
+        for i in range(len(nums)):
+
+            if (nums[i] > 0):
+                break
+            if (i > 0 and nums[i] == nums[i - 1]):
+                continue
+
+            cur_Tar = -(nums[i])
+            j, z = i + 1, len(nums) - 1
+            while j < z:
+                cur_Sum = nums[j] + nums[z]
+                if (cur_Sum == cur_Tar):
+                    ans.append((nums[i], nums[j], nums[z]))
+                    j += 1
+                    z -= 1
+                    while nums[j] == nums[j - 1] and j < z:
+                        j += 1
+                elif (cur_Sum < cur_Tar):
+                    j += 1
+                else:
+                    z -= 1
+        
+        return ans
+
+# Time Complexity: O(n^2)
+# Space Complexity: O(1) 
+
+# Result: Accepted
